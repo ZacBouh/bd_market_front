125dcf8492ea7ee391853b6e0dc6604a52f15905
ZacBouh <50382656+ZacBouh@users.noreply.github.com>
2025-09-28 23:09:09 +0000

    fixed auth guard

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
-export const userAtom = atomWithStorage<LoggedInUser | null>('user', null)
+export const userAtom = atomWithStorage<LoggedInUser | null>('user', null, undefined, {getOnInit: true})
 
 export const oAuthAtom = atom<any>(null)
