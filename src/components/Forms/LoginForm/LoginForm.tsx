d74aaf648de32c9be724aae38a5098c4ee47d468
ZacBouh <50382656+ZacBouh@users.noreply.github.com>
2025-10-05 18:30:54 +0000

    Adds validation on Login Form

--- src/components/Forms/LoginForm/LoginForm.tsx
+++ src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,42 +1,58 @@
 import { Box, Button,  TextField } from '@mui/material';
-import { useState } from 'react';
 import { loginUser } from '@/backend/api/auth';
 import { useNotifications } from '@toolpad/core/useNotifications';
+import { zodResolver } from "@hookform/resolvers/zod"
+import {  z } from "zod"
+import {useForm} from "react-hook-form"
+
+const schema = z.object({
+    email: z.email({error: " "}).nonempty("An email is required"),
+    password: z.string()
+        .nonempty(" ")
+        .min(8, "Min 8 characters")
+        .regex(/[A-Z]/, {error: "One uppercase"})
+        .regex(/[a-z]/, {error: "One lowercase"})
+        .regex(/\d/, "One number")
+        .regex(/^\S+$/, {error: "No space allowed"})
+        .regex(/[^A-Za-z0-9\s\/\\"'{}()<>|;:]/, {error: "One special character"})
+})
+
+export type LoginFormData = z.infer<typeof schema>
 
 const LoginForm = () => {
-    const initialState = {
-        email: '',
-        password: ''
-    }
-    const [loginForm, setLoginForm] = useState(initialState)
+    const {handleSubmit, register, formState : {errors}, reset} = useForm({
+        resolver: zodResolver(schema),
+        defaultValues: { email: '', password: '' },
+        mode: 'onChange'
+    })
     const notifications = useNotifications()
-    return <Box component='form'  onSubmit={async (event) => {
-            event.preventDefault()
-            notifications.show(<h1>"Form submitted"</h1>, {autoHideDuration: 5000})
-            const loginResponse = await loginUser(loginForm)
-            notifications.show(<h2>Login Response {loginResponse?.user.email}</h2>, {autoHideDuration: 5000})
-        }}
+    const onSubmit = async (data: LoginFormData) => {
+        notifications.show(<h1>"Form submitted"</h1>, {autoHideDuration: 5000})
+        const loginResponse = await loginUser(data)
+        notifications.show(<h2>Login Response {loginResponse?.user.email}</h2>, {autoHideDuration: 5000})
+    }
+    console.log(Object.keys(errors))
+    return <Box component='form'  onSubmit={handleSubmit(onSubmit, () => notifications.show("Invalid Credentials", {severity: 'error', autoHideDuration: 2500}))}
           sx={{
             width:'100%',
-
         }}
         >
             <TextField 
             label="email"
-            value={loginForm.email}
-            onChange={(event) => setLoginForm((loginForm) => ({...loginForm, email: event.target.value}))}
-            required
+            {...register('email')}
+            // required
             fullWidth
+            // helperText={errors.email?.message}
             />
             <TextField 
             label="password"
-            value={loginForm.password}
-            onChange={(event) => setLoginForm((loginForm) => ({...loginForm, password: event.target.value}))}
-            required
+            {...register('password')}
+            // required
             fullWidth
+            // helperText={errors.password?.message}
             />
             <Box sx={{display: 'grid', gridTemplateColumns:'1fr 1fr', gap: 1}} >
-                <Button onClick={() => setLoginForm(initialState)} >Reset</Button>
+                <Button onClick={() => reset()} >Reset</Button>
                 <Button type='submit' sx={{
                     position: 'relative',
                     zIndex: 9999,
